5f610303540ba292a9c2976c6f97d38f75046656
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-10-29 08:33:47 +0100

    Specify array by repeating the query key

--- src/tilestacker.js
+++ src/tilestacker.js
@@ -3,8 +3,7 @@ const stack = require("./stack");
 async function get(coords, query, config) {
   if (!query.layers)
     throw new Error('Query argument "layers" must be specified.');
-  const layers = query.layers.split(",");
-  const tile = await stack(config, layers, coords);
+  const tile = await stack(config, query.layers, coords);
   if (!tile) return null;
   tile.contentType = "image/png";
   return tile;
